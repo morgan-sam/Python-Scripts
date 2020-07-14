c515a339c8e430e8e4fc9465e341d515ddfd2200
Sam <samuel_morgan_@outlook.com>
2020-07-14 16:26:25 +0100

    Capitalisation of fns

--- inlineReactToCSS/inlineReactToCSS.py
+++ inlineReactToCSS/inlineReactToCSS.py
@@ -2,31 +2,38 @@ import os
 import re
 import os
 
-def findInlineStylesFromCSS(file):
+
+def findInlineStylesFromCss(file):
     regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
-    return re.findall(regex, file, re.MULTILINE|re.DOTALL)
+    return re.findall(regex, file, re.MULTILINE | re.DOTALL)
+
 
 def removeSpeechMarks(inlines):
-    return map(lambda x:re.sub(r'[\'\`]','',x),inlines)
+    return map(lambda x: re.sub(r'[\'\`]', '', x), inlines)
+
 
 def replaceCommasWithColons(inlines):
-    return map(lambda x:re.sub(r',\n',';\n',x),inlines)
+    return map(lambda x: re.sub(r',\n', ';\n', x), inlines)
+
 
 def removeCamelCase(inlines):
-    return map(lambda x:re.sub(r'([A-Z])',lambda x: '-'+x.group(0).lower(),x),inlines)
+    return map(lambda x: re.sub(r'([A-Z])', lambda x: '-'+x.group(0).lower(), x), inlines)
+
 
 def convertToClassName(inlines):
-    return map(lambda x:re.sub(r'(?:export )*const ([a-zA-z0-9\-]+) = ','.\\1 ',x),inlines)
+    return map(lambda x: re.sub(r'(?:export )*const ([a-zA-z0-9\-]+) = ', '.\\1 ', x), inlines)
 
-def inlineStylesToCSS(styles):
+
+def inlineStylesToCss(styles):
     styles = removeSpeechMarks(styles)
     styles = replaceCommasWithColons(styles)
     styles = removeCamelCase(styles)
     return convertToClassName(styles)
 
-def convertInlinesToCssFile(file,inlines):
-    newFileName=re.sub(r'.js','.css',file)
-    styles=inlineStylesToCSS(inlines)
+
+def convertInlinesToCssFile(file, inlines):
+    newFileName = re.sub(r'.js', '.css', file)
+    styles = inlineStylesToCss(inlines)
     with open(os.path.join(currentpath, newFileName), 'w') as f:
         for style in styles:
             f.write("%s\n" % style)
@@ -40,9 +47,9 @@ for currentpath, folders, files in os.walk('./input'):
         path = (os.path.join(currentpath, file))
         open_file = open(path, 'r')
         read_file = open_file.read()
-        inlines = findInlineStylesFromCSS(read_file)
+        inlines = findInlineStylesFromCss(read_file)
         for match in inlines:
-            read_file = read_file.replace(match,'')
+            read_file = read_file.replace(match, '')
         write_file = open(path, 'w')
         write_file.write(read_file)
-        convertInlinesToCssFile(file,inlines)
+        convertInlinesToCssFile(file, inlines)
