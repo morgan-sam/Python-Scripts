7b7951a02cd62f6f12735ec4f1b6ecf0ff968c54
Sam <samuel_morgan_@outlook.com>
2020-07-14 14:44:57 +0100

    findInlineStylesFromCSS fn

--- inlineReactToCSS/inlineReactToCSS.py
+++ inlineReactToCSS/inlineReactToCSS.py
@@ -1,17 +1,20 @@
 import os
 import re
 
+
+def findInlineStylesFromCSS(file):
+    regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
+    return re.findall(regex, file, re.MULTILINE|re.DOTALL)
+
+
 for currentpath, folders, files in os.walk('./input'):
     for file in files:
         path = (os.path.join(currentpath, file))
         open_file = open(path, 'r')
         read_file = open_file.read()
-        print(read_file)
-        regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
-        matches = re.findall(regex, read_file, re.MULTILINE|re.DOTALL)
+        matches = findInlineStylesFromCSS(read_file)
         for match in matches:
-            read_file = read_file.replace(match,'')            
-        print(read_file)
-        print(matches)
+            read_file = read_file.replace(match,'')
         write_file = open(path, 'w')
         write_file.write(read_file)
+
