5b4dc953c5bdc5893474513bbf8262fba6099656
Sam <samuel_morgan_@outlook.com>
2020-07-14 14:40:55 +0100

    Pops all const matches in arr

--- inlineReactToCSS/inlineReactToCSS.py
+++ inlineReactToCSS/inlineReactToCSS.py
@@ -6,11 +6,12 @@ for currentpath, folders, files in os.walk('./input'):
         path = (os.path.join(currentpath, file))
         open_file = open(path, 'r')
         read_file = open_file.read()
-
-        # format obj name / classname
-        regex = re.compile(
-            '(?:export )*const ([a-zA-z0-9]+) = ({.+?(?=};)};)', flags=re.MULTILINE | re.DOTALL)
-        read_file = regex.sub('\\1: \\2', read_file)
-
+        print(read_file)
+        regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
+        matches = re.findall(regex, read_file, re.MULTILINE|re.DOTALL)
+        for match in matches:
+            read_file = read_file.replace(match,'')            
+        print(read_file)
+        print(matches)
         write_file = open(path, 'w')
         write_file.write(read_file)
