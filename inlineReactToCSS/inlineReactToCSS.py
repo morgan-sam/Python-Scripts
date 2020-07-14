35979754acc57394d40144a847d2bbebb2a3d427
Sam <samuel_morgan_@outlook.com>
2020-07-14 18:15:32 +0100

    getDirectory fn

--- inlineReactToCSS/inlineReactToCSS.py
+++ inlineReactToCSS/inlineReactToCSS.py
@@ -1,28 +1,27 @@
 import os
 import re
 import os
+import sys
 
+def getDirectory():
+    if len(sys.argv) > 1: return sys.argv[1]
+    else: return '.'
 
 def findInlineStylesFromCss(file):
     regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
     return re.findall(regex, file, re.MULTILINE | re.DOTALL)
 
-
 def removeSpeechMarks(inlines):
     return map(lambda x: re.sub(r'[\'\`]', '', x), inlines)
-
 
 def replaceCommasWithColons(inlines):
     return map(lambda x: re.sub(r',\n', ';\n', x), inlines)
 
-
 def removeCamelCase(inlines):
     return map(lambda x: re.sub(r'([A-Z])', lambda x: '-'+x.group(0).lower(), x), inlines)
 
-
 def convertToClassName(inlines):
     return map(lambda x: re.sub(r'(?:export )*const ([a-zA-z0-9\-]+) = ', '.\\1 ', x), inlines)
-
 
 def inlineStylesToCss(styles):
     styles = removeSpeechMarks(styles)
@@ -30,14 +29,12 @@ def inlineStylesToCss(styles):
     styles = removeCamelCase(styles)
     return convertToClassName(styles)
 
-
 def convertInlinesToCssFile(file, inlines):
     newFileName = re.sub(r'.js', '.css', file)
     styles = inlineStylesToCss(inlines)
     with open(os.path.join(currentpath, newFileName), 'w') as f:
         for style in styles:
             f.write("%s\n" % style)
-
 
 os.system("./resetInput.sh")
 os.system("prettier --write ./input/*.js")
