057994f6eefdac8f6a5787581fea144eba2aa414
Sam <samuel_morgan_@outlook.com>
2020-07-15 12:58:34 +0100

    findConstantsUsedByInlines fn

--- inlineReactToCSS/inlineReactToCSS.py
+++ inlineReactToCSS/inlineReactToCSS.py
@@ -1,34 +1,53 @@
-import os, re, sys
+import os
+import re
+import sys
+
 
 def getDirectory():
-    if len(sys.argv) > 1: return sys.argv[1]
-    else: return '.'
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return '.'
+
 
 def findConstantsFromCss(file):
     regex = r'(?:export )*const ([A-Z_]+) = ([a-zA-Z0-9_\-\'\"\`]+);'
     return re.findall(regex, file, re.MULTILINE | re.DOTALL)
 
+
+def findConstantsUsedByInlines(inlines, constants):
+    varNames = map(lambda x: x[0], constants)
+    allInlines = ''.join(inlines)
+    return filter(lambda x: allInlines.find(x) != -1, varNames)
+
+
 def findInlineStylesFromCss(file):
     regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
     return re.findall(regex, file, re.MULTILINE | re.DOTALL)
 
+
 def removeSpeechMarks(inlines):
     return map(lambda x: re.sub(r'[\'\`]', '', x), inlines)
+
 
 def replaceCommasWithColons(inlines):
     return map(lambda x: re.sub(r',\n', ';\n', x), inlines)
 
+
 def removeCamelCase(inlines):
     return map(lambda x: re.sub(r'([A-Z])', lambda x: '-'+x.group(0).lower(), x), inlines)
 
+
 def convertToClassName(inlines):
     return map(lambda x: re.sub(r'(?:export )*const ([a-zA-z0-9\-]+) = ', '.\\1 ', x), inlines)
+
 
 def inlineStylesToCss(styles):
     styles = removeSpeechMarks(styles)
     styles = replaceCommasWithColons(styles)
     styles = removeCamelCase(styles)
     return convertToClassName(styles)
+
 
 def convertInlinesToCssFile(file, inlines):
     if (len(inlines)):
@@ -37,6 +56,7 @@ def convertInlinesToCssFile(file, inlines):
         with open(os.path.join(currentpath, newFileName), 'a+') as f:
             for style in styles:
                 f.write("%s\n\n" % style)
+
 
 dir = getDirectory()
 os.system('prettier --write {}/*.js'.format(dir))
@@ -48,12 +68,14 @@ for currentpath, folders, files in os.walk(dir):
             open_file = open(path, 'r')
             read_file = open_file.read()
             constants = findConstantsFromCss(read_file)
-            print(constants)
             inlines = findInlineStylesFromCss(read_file)
+            constUsed = findConstantsUsedByInlines(inlines, constants)
+            print(constUsed)
             for match in inlines:
                 read_file = read_file.replace(match, '').strip()
             if (len(read_file) > 0):
                 write_file = open(path, 'w')
                 write_file.write(read_file)
-            else: os.remove(path)
+            else:
+                os.remove(path)
             convertInlinesToCssFile(file, inlines)
--- inlineReactToCSS/input/default.js
+++ inlineReactToCSS/input/default.js
@@ -1,8 +1,10 @@
 export const EVENT_HEIGHT_REM = 5;
 export const BUTTON_COLOR = 'salmon';
+export const DATE_SELECT_COLOR = 'red';
 
 export const dateSelectStyle = {
     display: 'grid',
+    backgroundColor: DATE_SELECT_COLOR,
     gridTemplateColumns: '3rem 4rem 5rem',
     gridTemplateRows: '1fr 3fr',
     gridTemplateAreas: '". . ." ". . ."',
