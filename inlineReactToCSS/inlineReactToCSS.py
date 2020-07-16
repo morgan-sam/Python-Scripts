4c6cf2c7953d0182ef871ad9627185558fd59c5a
Sam <samuel_morgan_@outlook.com>
2020-07-16 11:44:43 +0100

    Moved formatIfInlineSingleLine to correct function

--- inlineReactToCSS/inlineReactToCSS.py
+++ inlineReactToCSS/inlineReactToCSS.py
@@ -26,21 +26,18 @@ def usedConstInlines(inlines, constants):
     return {key: constants[key] for key in used}
 
 
+
+def findInlineStylesFromCss(file):
+    regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
+    return re.findall(regex, file, re.MULTILINE | re.DOTALL)
+
+
 def formatIfInlineSingleLine(inline):
     if(len(re.findall(r'\n', inline, re.MULTILINE))==0):
         regex = r'(?![a-zA-Z0-9, ]+\(),(?![a-zA-Z0-9, ]+\))'
         inline = re.sub(regex, ';', inline)
     return inline
-
-
-def findInlineStylesFromCss(file):
-    regex = r'(?:export )*const [a-zA-z0-9]+ = {.+?(?=};)};'
-    inlines = re.findall(regex, file, re.MULTILINE | re.DOTALL)
-    inlines = map(formatIfInlineSingleLine, inlines)
-    for inline in inlines:
-        print(inline)
-    return inlines
-
+    
 def addCssVarsToInline(inline, constDic):
     keys = list(constDic.keys())
     for i in range(len(keys)):
@@ -68,6 +65,7 @@ def constToCssVarFormat(const):
 
 
 def inlineStylesToCss(styles, constDic):
+    styles = map(formatIfInlineSingleLine, styles)
     styles = map(lambda x: addCssVarsToInline(x, constDic), styles)
     styles = removeSpeechMarks(styles)
     styles = replaceCommasWithColons(styles)
