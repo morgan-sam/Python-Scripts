2200455592255614cf372c276ec385babce3536a
Sam <samuel_morgan_@outlook.com>
2020-07-18 14:06:01 +0100

    Removed faulty example inline js

--- inlineReactToCSS/input/default.js
+++ inlineReactToCSS/input/default.js
@@ -1,8 +1,7 @@
-const EVENT_HEIGHT_REM = 2;
 const zzwidth = 3;
 const zzcolor = '#FF7744';
 export const currentTimeMarkerStyle = {
-    width: `calc(${EVENT_HEIGHT_REM + 3}rem - 2px`,
+    width: `calc(${EVENT_HEIGHT_REM + 3}rem - 2px)`,
     height: `${zzwidth * 2}px`,
     background: `linear-gradient(-45deg, ${zzcolor} ${zzwidth}px, transparent 0), linear-gradient(45deg, ${zzcolor} ${zzwidth}px, transparent 0)`,
     backgroundSize: `${zzwidth * 2}px ${zzwidth * 2}px`,
