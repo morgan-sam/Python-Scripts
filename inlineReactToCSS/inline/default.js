f0cca5b76bc6af68f0d281a615f42f80f68bee7d
Sam <samuel_morgan_@outlook.com>
2020-07-19 09:50:16 +0100

    Added broken test style

--- inlineReactToCSS/inline/default.js
+++ inlineReactToCSS/inline/default.js
@@ -1,14 +1,20 @@
-const zzwidth = 3;
-const zzcolor = '#FF7744';
-export const currentTimeMarkerStyle = {
-	width: `calc(${EVENT_HEIGHT_REM + 3}rem - 2px)`,
-	height: `${zzwidth * 2}px`,
-	background: `linear-gradient(-45deg, ${zzcolor} ${zzwidth}px, transparent 0), linear-gradient(45deg, ${zzcolor} ${zzwidth}px, transparent 0)`,
-	backgroundSize: `${zzwidth * 2}px ${zzwidth * 2}px`,
-	backgroundPosition: 'left-center',
-	transformOrigin: `-1px 0%`,
-	position: 'absolute',
-	top: '0',
-	right: '0',
-	transform: `rotate(90deg) translateY(-${zzwidth * 2}px)`
+import { remToPx } from 'utility/convertUnit';
+
+export const GRAPH_SIZES = {
+	TOP_GAP: remToPx(5),
+	BOTTOM_GAP: remToPx(5),
+	LEFT_GAP: remToPx(5),
+	RIGHT_GAP: remToPx(3),
+	FONT_SIZE: remToPx(1)
+};
+
+export const formStyle = {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center'
+};
+
+export const formInputStyle = {
+	margin: '0.6rem 0'
 };
